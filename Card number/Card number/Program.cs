c4a613a7466e3620ad7941274cf0f61eebd7279a
Nirmalnk30 <114048163+Nirmalnk30@users.noreply.github.com>
2022-10-27 17:15:49 +0530

    Add files via upload

---
+++ Card number/Card number/Program.cs
@@ -0,0 +1,22 @@
+﻿using Card_number;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+
+class program
+{
+    public static void Main(string[] val)
+    {
+        Console.WriteLine("Welcome to redit card number checker ");
+        Console.WriteLine("Enter your credit card number");
+        long ccnumber =(long)Convert.ToDouble(Console.ReadLine());
+        
+        Checkers ck = new Checkers();
+
+        // ck.Checkcc();
+        ck.ReverseCCNumber(ccnumber);
+       
+        
+
+    }
+}
+
